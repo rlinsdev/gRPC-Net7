f59ccb5ffadb43bcb04063e3b0da522a9b7c556d
Rodrigo Lins <rlinsdev@gmail.com>
2023-08-04 16:17:06 -0300

    Add Model

---
+++ Models/ToDoItem.cs
@@ -0,0 +1,13 @@
+namespace ToDoGrpc.Models;
+
+public class ToDoItem
+{
+    public int id { get; set; }
+
+    public string? Title { get; set; }
+
+    public string? Description { get; set; }	
+    
+    public string ToDoStatus { get; set; } = "NEW";
+
+}
