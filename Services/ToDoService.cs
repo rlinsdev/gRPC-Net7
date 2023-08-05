f6c1792b2e65dadb5f6a06e75146ca9f297fe4da
Rodrigo Lins <rlinsdev@gmail.com>
2023-08-05 08:45:26 -0300

    ToDoService

---
+++ Services/ToDoService.cs
@@ -0,0 +1,39 @@
+using Grpc.Core;
+using ToDoGrpc.Data;
+using ToDoGrpc.Models;
+
+namespace ToDoGrpc.Services;
+
+public class ToDoService: ToDoIt.ToDoItBase
+{
+    private AppDbContext _dbContext;
+
+    public ToDoService(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public override async Task<CreateToDoResponse> CreateToDo(CreateToDoRequest request, ServerCallContext context)
+    {
+        if (request.Title == string.Empty || request.Description == string.Empty)
+        {
+            throw new RpcException(new Status(StatusCode.InvalidArgument, "You Must Supply a valid object"));
+        }
+
+        var toDoItem = new ToDoItem
+        {
+            Title = request.Title,
+            Description = request.Description,
+
+        };
+
+        await _dbContext.AddAsync(toDoItem);
+        await _dbContext.SaveChangesAsync();
+
+        return await Task.FromResult(new CreateToDoResponse{
+            Id = toDoItem.Id
+
+        });
+
+    }
+}
